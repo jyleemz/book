046511201180415c1be15486245dc2a96bb1e1d9
jyleemz <yubbi@mz.co.kr>
2022-09-26 17:11:15 +0900

    Initial commit

---
+++ src/main/java/com/springboot/book/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.springboot.book.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
---
+++ src/main/java/com/springboot/book/controller/PutContoller.java
@@ -0,0 +1,46 @@
+package com.springboot.book.controller;
+
+import com.springboot.book.dto.MemberDto;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/put-api")
+public class PutContoller {
+
+    @PutMapping(value = "/member")
+    public String postMember(@RequestBody Map<String, Object> putData) {
+        StringBuilder sb = new StringBuilder();
+
+        putData.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+
+        return sb.toString();
+    }
+
+    @PutMapping(value = "/member1")
+    public String postMamberDto1(@RequestBody MemberDto memberDto) {
+        return memberDto.toString();
+    }
+
+    @PutMapping(value = "/member2")
+    public MemberDto postMemberDto2(@RequestBody MemberDto memberDto) {
+        return memberDto;
+    }
+
+    @PutMapping(value = "/member3")
+    public ResponseEntity<MemberDto> postMemberDto3(@RequestBody MemberDto memberDto) {
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body(memberDto);
+    }
+
+}
